c0da5e3366be2e352815508f22d28e67937568b6
jasondavindev <jasondavin.dev@gmail.com>
2021-09-23 00:12:48 -0300

    add alias

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,13 @@ echo Modifying Kubernetes config to point to Kind master node
 MASTER_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-cluster-control-plane)
 sed -i "s/^    server:.*/    server: https:\/\/$MASTER_IP:6443/" $HOME/.kube/config
 
-cat > ~/.bashrc <<EOF
+cat >> ~/.bashrc <<EOF
 alias k="kubectl"
 alias kg="kubectl get"
 alias kd="kubectl describe"
 alias ke="kubectl exec -ti"
 alias krm="kubectl delete"
+alias kl="kubectl logs"
 alias ka="kubectl apply -f"
 alias kns="kubens"
 alias kctx="kubectx"
